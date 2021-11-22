074b5149f71001ebe1cfd09341a47e40b4cbe24e
dizzyplay <vovoboss@gmail.com>
2021-11-22 21:58:24 +0900

    fommating

--- src/leetcode/climb_stairs.rs
+++ src/leetcode/climb_stairs.rs
@@ -23,14 +23,14 @@ impl Solution {
         dp[n - 1]
     }
 
-    pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32{
+    pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
         if cost.len() == 1 {
             return cost[0];
         }
         let mut answer = vec![0; cost.len() + 1];
         for i in 2..=cost.len() as usize {
-            let one = answer[i-2] + cost[i-2];
-            let two = answer[i-1] + cost[i-1];
+            let one = answer[i - 2] + cost[i - 2];
+            let two = answer[i - 1] + cost[i - 1];
             answer[i] = one.min(two);
         }
         answer[answer.len() - 1]
@@ -49,6 +49,6 @@ mod tests {
         assert_eq!(Solution::solution2(5), 8);
         assert_eq!(Solution::solution2(6), 13);
 
-        assert_eq!(Solution::min_cost_climbing_stairs(vec![10,15,20]), 15);
+        assert_eq!(Solution::min_cost_climbing_stairs(vec![10, 15, 20]), 15);
     }
 }
