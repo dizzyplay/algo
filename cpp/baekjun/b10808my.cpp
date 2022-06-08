70c713123d208bce9cdb5d71931d2fa0d9040dd2
dizzyplay <vovoboss@gmail.com>
2022-06-08 22:31:57 +0900

    ok

--- cpp/baekjun/b10808my.cpp
+++ cpp/baekjun/b10808my.cpp
@@ -4,6 +4,7 @@ using namespace std;
 string s = "baekjook";
 
 int main(){
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	map<char, int> m;
 	for(int i =0; i<26; i++){
 		m[(char)i+97] = 0;
