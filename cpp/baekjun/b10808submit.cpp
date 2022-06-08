f43104065c9e532ce9f40ee2c02633da1b0ae9c8
dizzyplay <vovoboss@gmail.com>
2022-06-08 22:36:42 +0900

    mysubmit

---
+++ cpp/baekjun/b10808submit.cpp
@@ -0,0 +1,13 @@
+#include <map>
+#include <iostream>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	string s;
+	cin >> s;
+	map<char, int> m;
+	for(int i=0; i < 25; i++) m[(char)i+97] = 0;
+	for(char c : s) m[c]++;
+	for(auto v: m) cout << v.second << " ";
+	return 0;
+}
