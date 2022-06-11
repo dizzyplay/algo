15b949adb651f17565b9de89edc4e00f39107249
dizzyplay <vovoboss@gmail.com>
2022-06-11 15:53:21 +0900

    ok

--- cpp/baekjun/b10988/b10988my.cpp
+++ cpp/baekjun/b10988/b10988my.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 string s;
 bool palin(int a, int b){
+	if (a >= b) return true;
 	if (s[a] != s[b] ) return false;
-	if (a == b-1) return true;
-	if (a ==b) return true;
 	return palin(a+1, b-1);
 }
 
 int main(){
-	cin >> s;
-	cout << palin(0,s.size() -1);
+	while(1){
+		cin >> s;
+		cout << palin(0,s.size() -1) << "\n";
+	}
 	return 0;
 }
