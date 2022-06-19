6e9a433f612159c253b51aedddfcbb311f8780a0
dizzyplay <vovoboss@gmail.com>
2022-06-20 01:04:23 +0900

    speed up

--- cpp/baekjun/b1940/b1940my.cpp
+++ cpp/baekjun/b1940/b1940my.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int n,k,ret;
 map<int,int> _map;
 int main(){
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> n >> k;
 	for(int i=0; i<n; i++){
 		int temp;
