fd1e8568d87b0ba9ee59c09c21f79c0e41045b9d
dizzyplay <vovoboss@gmail.com>
2022-06-20 22:49:45 +0900

    fix

--- cpp/baekjun/b3986/b3986my.cpp
+++ cpp/baekjun/b3986/b3986my.cpp
@@ -2,19 +2,16 @@
 using namespace std;
 
 int n,ret;
+string s;
 
 int main(){
 	cin >> n;
 	for(int i=0; i<n; i++){
-		string s;
 		stack<char> _stack;
 		cin >> s;
 		for(auto c: s){
-			if (_stack.size() == 0) _stack.push(c);
-			else {
-				if (_stack.top() == c) _stack.pop();
-				else _stack.push(c);
-			}
+			if (_stack.size() && _stack.top() == c) _stack.pop();
+			else _stack.push(c);
 		}
 		if(_stack.empty()) ret++;
 	}
